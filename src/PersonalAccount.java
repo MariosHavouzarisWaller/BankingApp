42c1cff59adb68a80dd7cc9b0a1ca779d5c40e09
MHWaller <106393304+MariosHavouzarisWaller@users.noreply.github.com>
2022-09-08 13:08:11 +0100

    Created Account super class

--- src/PersonalAccount.java
+++ src/PersonalAccount.java
@@ -1,2 +1,6 @@
-public class PersonalAccount {
+// This object is for the Personal Account abstract class
+public class PersonalAccount extends Account {
+    public PersonalAccount(long accountNumber, long sortCode, char accountType, double userFunds, boolean directDebit, boolean standingOrder, boolean tax) {
+        super(accountNumber, sortCode, accountType, userFunds, directDebit, standingOrder, tax);
+    }
 }
