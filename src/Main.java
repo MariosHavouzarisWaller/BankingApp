65b2398c66bda7eb5194eed4f45639278a32c2ab
MHWaller <106393304+MariosHavouzarisWaller@users.noreply.github.com>
2022-09-13 17:27:25 +0100

    Default info created

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 // This will act as the main class where we initialise everything in the Banking App
 public class Main {
 
     public static void main(String[] args) {
+        // PERSONAL ACCOUNT DEFAULT DATA SECTION START
+        // This creates default Personal Account info for the admin to check
+        List paList = new ArrayList<User>();
+        Scanner scan = new Scanner(System.in);
 
+        PersonalAccount pa1 = new PersonalAccount(1, "tempPhotoId", "tempAddressId", 1,
+                2, 3, 2, 1, 'd', 5,
+                false, false, false, 5, false, 5);
+        PersonalAccount pa2 = new PersonalAccount(2, "tempPhotoId", "tempAddressId", 1,
+                2, 3, 2, 1, 'd', 5,
+                false, false, false, 5, false, 5);
+        PersonalAccount pa3 = new PersonalAccount(3, "tempPhotoId", "tempAddressId", 1,
+                2, 3, 2, 1, 'd', 5,
+                false, false, false, 5, false, 5);
+        paList.add(pa1);
+        paList.add(pa2);
+        paList.add(pa3);
+
+        // Output the results of one of the Personal Accounts created
+        System.out.println("ID: " + pa1.getId() + "\nPhoto ID: " + pa1.getPhotoId() + "\nAddress ID:" + pa1.getAddressId() + "\nNumber of Personal Accounts: " + pa1.getNoOfPersonalAcc() + "\nNumber of ISA Accounts: " +
+                pa1.getNoOfISAAcc() + "\nNumber of Business Accounts: " + pa1.getNoOfBusinessAcc() + "\nAccount Number: " + pa1.getAccountNumber() + "\nSort Code: " + pa1.getSortCode() + "\nTax: " +
+                pa1.getAccountType() + "\nUser Funds: " + pa1.getUserFunds() + "\nDirect Debit: " + pa1.isDirectDebit() + "\nStanding Orders: " + pa1.isStandingOrder() + "\nTax: " +
+                pa1.isTax() + "\nMinimum Charge: " + pa1.getMinimumCharge() + "\nDebt: " + pa1.isInDebt() + "\nCharges: " + pa1.getCharges() + "\n\n\n");
+
+        // PERSONAL ACCOUNT DEFAULT DATA SECTION END
     }
 }
--- src/PersonalAccount.java
+++ src/PersonalAccount.java
@@ -1,6 +1,52 @@
 // This object is for the Personal Account abstract class
 public class PersonalAccount extends Account {
-    public PersonalAccount(long accountNumber, long sortCode, char accountType, double userFunds, boolean directDebit, boolean standingOrder, boolean tax) {
-        super(accountNumber, sortCode, accountType, userFunds, directDebit, standingOrder, tax);
+        private double minimumCharge;
+        private boolean inDebt;
+        private double charges;
+
+        // Constructor
+    public PersonalAccount(long id, String photoId, String addressId, int noOfPersonalAcc,
+        int noOfISAAcc, int noOfBusinessAcc, long accountNumber, long sortCode,
+        char accountType, double userFunds, boolean directDebit, boolean standingOrder,
+        boolean tax, double minimumCharge, boolean inDebt, double charges) {
+            super(id, photoId, addressId, noOfPersonalAcc, noOfISAAcc, noOfBusinessAcc, accountNumber, sortCode, accountType, userFunds, directDebit, standingOrder, tax);
+            this.minimumCharge = minimumCharge;
+            this.inDebt = inDebt;
+            this.charges = charges;
+        }
+
+    // Getter's and Setter's
+    public double getMinimumCharge() {
+        return minimumCharge = 1.0;
+    }
+
+    public void setMinimumCharge(double minimumCharge) {
+        this.minimumCharge = minimumCharge;
+    }
+
+    public boolean isInDebt() {
+        if (getUserFunds() < 0) {
+            return inDebt = true;
+        }
+        else {
+            return inDebt = false;
+        }
+    }
+
+    public void setInDebt(boolean inDebt) {
+        this.inDebt = inDebt;
+    }
+
+    public double getCharges() {
+        if (isInDebt() == true) {
+            charges = getUserFunds() * (-0.3);
+            return charges;
+        } else {
+            return 0;
+        }
+    }
+
+    public void setCharges(double charges) {
+        this.charges = charges;
     }
 }
