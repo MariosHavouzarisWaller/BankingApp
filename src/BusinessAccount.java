1f62b5fb96832e73273af22f2e509a38cb4745c9
vidhisha3 <vidhishapatel@yahoo.com>
2022-09-13 15:02:54 +0100

    commit BusinessAccount.java

--- src/BusinessAccount.java
+++ src/BusinessAccount.java
@@ -1,2 +1,26 @@
-public class BusinessAccount {
+public class BusinessAccount extends Account{
+
+    private boolean businessProof;
+
+    private String bannedBuisness;
+
+    private String businessSector;
+
+    private boolean chequebookReq;
+
+    private double overdraft;
+
+    private double loanValue;
+
+    public BusinessAccount(long accountNumber, long sortCode, char accountType, double userFunds, boolean directDebit, boolean standingOrder, boolean tax){
+        super(accountNumber, sortCode, accountType, userFunds, directDebit, standingOrder, tax);
+        this.businessProof = businessProof;
+        this.bannedBuisness = bannedBuisness;
+        this.businessSector = businessSector;
+        this.chequebookReq = chequebookReq;
+        this.overdraft = overdraft;
+        this.loanValue = loanValue;
+    }
+
 }
+
