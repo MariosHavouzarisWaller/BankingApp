0156cb3dfb551c38e51fce206e5328f496ef30cd
MHWaller <106393304+MariosHavouzarisWaller@users.noreply.github.com>
2022-09-20 18:31:29 +0100

    Kinda fixed all the issues

--- src/BusinessAccount.java
+++ src/BusinessAccount.java
@@ -1,15 +1,15 @@
 public class BusinessAccount extends Account{
     private boolean businessProof;
-    private String bannedBuisness;
+    private String bannedBusiness;
     private String businessSector;
     private boolean chequebookReq;
     private double overdraft;
     private double loanValue;
 
-    public BusinessAccount(long accountNumber, long sortCode, char accountType, double userFunds, boolean directDebit, boolean standingOrder, boolean tax){
+    public BusinessAccount(long id, String photoId, String addressId, int noOfPersonalAcc, int noOfISAAcc, int noOfBusinessAcc, long accountNumber, long sortCode, char accountType, double userFunds, boolean directDebit, boolean standingOrder, boolean tax, boolean businessProof, String bannedBusiness, String businessSector, boolean chequebookReq, double overdraft, double loanValue) {
         super(id, photoId, addressId, noOfPersonalAcc, noOfISAAcc, noOfBusinessAcc, accountNumber, sortCode, accountType, userFunds, directDebit, standingOrder, tax);
         this.businessProof = businessProof;
-        this.bannedBuisness = bannedBuisness;
+        this.bannedBusiness = bannedBusiness;
         this.businessSector = businessSector;
         this.chequebookReq = chequebookReq;
         this.overdraft = overdraft;
@@ -18,8 +18,8 @@ public class BusinessAccount extends Account{
     public boolean getBusinessProof()
     {return businessProof;}
     public void setBusinessProof(boolean businessProof){this.businessProof = businessProof;}
-    public String getBannedBuisness(){return bannedBuisness;}
-    public void setBannedBuisness (String bannedBuisness){this.bannedBuisness = bannedBuisness;}
+    public String getBannedBuisness(){return bannedBusiness;}
+    public void setBannedBuisness (String bannedBuisness){this.bannedBusiness = bannedBuisness;}
     public String getBusinessSector (){return businessSector;}
     public void setBusinessSector(String businessSector){this.businessSector = businessSector;}
     public boolean getChequebookReq (){return chequebookReq;}
--- src/Main.java
+++ src/Main.java
@@ -9,63 +9,106 @@ public class Main {
         // PERSONAL ACCOUNT DEFAULT DATA SECTION START
         // This creates default Personal Account info for the admin to check
         List paList = new ArrayList<User>();
-        Scanner scan = new Scanner(System.in);
 
-        PersonalAccount pa1 = new PersonalAccount(1, "tempPhotoId", "tempAddressId", 1,
-                2, 3, 2, 1, 'd', 5,
-                false, false, false, 5, false, 5);
-        PersonalAccount pa2 = new PersonalAccount(2, "tempPhotoId", "tempAddressId", 1,
-                2, 3, 2, 1, 'd', 5,
-                false, false, false, 5, false, 5);
-        PersonalAccount pa3 = new PersonalAccount(3, "tempPhotoId", "tempAddressId", 1,
-                2, 3, 2, 1, 'd', 5,
-                false, false, false, 5, false, 5);
+        PersonalAccount pa1 = new PersonalAccount(1, "tempPhotoId", "tempAddressId", 1, 2, 3, 2, 1, 'd', 5, false, false, false, 5, false, 5);
+        PersonalAccount pa2 = new PersonalAccount(2, "tempPhotoId", "tempAddressId", 1, 2, 3, 2, 1, 'd', 5, false, false, false, 5, false, 5);
+        PersonalAccount pa3 = new PersonalAccount(3, "tempPhotoId", "tempAddressId", 1, 2, 3, 2, 1, 'd', 5, false, false, false, 5, false, 5);
 
-        ISAAccount pa4 = new ISAAccount(4,"tempPhotoId", "tempAddressId", 1, 3,
-                0, 1, 1, 'd', 5, true, false, false,
-                true, false, true, true, false, 15000, 22);
-        ISAAccount pa5 = new ISAAccount(5,"tempPhotoId", "tempAddressId", 1, 4,
-                0, 1, 1, 'd', 5, false, false, false,
-                true, true, true, true, true, 7777, 40);
-        ISAAccount pa6 = new ISAAccount(6,"tempPhotoId", "tempAddressId", 1, 2,
-                2, 4, 1, 'd', 5, true, false, false,
-                true, false, true, false, false, 2000, 69);
         paList.add(pa1);
         paList.add(pa2);
         paList.add(pa3);
-        paList.add(pa4);
-        paList.add(pa5);
-        paList.add(pa6);
 
-        // Output the results of one of the Personal Accounts created
-        System.out.println("ID: " + pa1.getId() + "\nPhoto ID: " + pa1.getPhotoId() + "\nAddress ID:" + pa1.getAddressId() + "\nNumber of Personal Accounts: " + pa1.getNoOfPersonalAcc() + "\nNumber of ISA Accounts: " +
-                pa1.getNoOfISAAcc() + "\nNumber of Business Accounts: " + pa1.getNoOfBusinessAcc() + "\nAccount Number: " + pa1.getAccountNumber() + "\nSort Code: " + pa1.getSortCode() + "\nTax: " +
-                pa1.getAccountType() + "\nUser Funds: " + pa1.getUserFunds() + "\nDirect Debit: " + pa1.isDirectDebit() + "\nStanding Orders: " + pa1.isStandingOrder() + "\nTax: " +
-                pa1.isTax() + "\nMinimum Charge: " + pa1.getMinimumCharge() + "\nDebt: " + pa1.isInDebt() + "\nCharges: " + pa1.getCharges() + "\n\n\n");
+        // Output the results of one of the Personal Accounts created (test)
+//        System.out.println("ID: " + pa1.getId() + "\nPhoto ID: " + pa1.getPhotoId() + "\nAddress ID:" + pa1.getAddressId() + "\nNumber of Personal Accounts: " + pa1.getNoOfPersonalAcc() + "\nNumber of ISA Accounts: " +
+//                pa1.getNoOfISAAcc() + "\nNumber of Business Accounts: " + pa1.getNoOfBusinessAcc() + "\nAccount Number: " + pa1.getAccountNumber() + "\nSort Code: " + pa1.getSortCode() + "\nTax: " +
+//                pa1.getAccountType() + "\nUser Funds: " + pa1.getUserFunds() + "\nDirect Debit: " + pa1.isDirectDebit() + "\nStanding Orders: " + pa1.isStandingOrder() + "\nTax: " +
+//                pa1.isTax() + "\nMinimum Charge: " + pa1.getMinimumCharge() + "\nDebt: " + pa1.isInDebt() + "\nCharges: " + pa1.getCharges() + "\n\n\n");
 
 
 
         // PERSONAL ACCOUNT DEFAULT DATA SECTION END
-        //Buisness Account DEfault Data
-        List baList = new ArrayList<User>();
-        Scanner scan = new Scanner(System.in);
 
-        BusinessAccount ba1 = new BuisnessAccount(1,"tempPhotoId", "tempAddressId", true, "tempBuisnessProof","tempBannedBuisness","private", true,10000,5000);
-        BusinessAccount ba2 = new BuisnessAccount(2,"tempPhotoId","tempAddressId", true, "tempBuisnessProof", "no", "private", true,15000,1200);
-        BusinessAccount ba3 = new BuisnessAccount(3,"tempPhotoId","tempAddressId", true, "tempBuisnessProof", "no", "private", true,1000,800);
+        // Business Account Default Data
+        List baList = new ArrayList<User>();
+
+        BusinessAccount ba1 = new BusinessAccount(1,"tempPhotoId", "tempAddressId", 1, 2, 1, 2432, 432, 'b',32, true,  true,  true, true,"tempBannedBusiness","private", true,10000,5000);
+        BusinessAccount ba2 = new BusinessAccount(2,"tempPhotoId", "tempAddressId", 1, 2, 1, 2432, 432, 'b',32, true,  true,  true, true,"tempBannedBusiness","private", true,10000,5000);
+        BusinessAccount ba3 = new BusinessAccount(3,"tempPhotoId", "tempAddressId", 1, 2, 1, 2432, 432, 'b',32, true,  true,  true, true,"tempBannedBusiness","private", true,10000,5000);
 
         baList.add(ba1);
         baList.add(ba2);
         baList.add(ba3);
-        //output the results of default business Account
-        System.out.println("ID:" + ba1.getId() + "\nPhoto ID: " + ba1.getPhotoId() + "\nAddress ID:" + ba1.getAddressId() + "\nNumber of Personal Accounts: " + ba1.getNoOfPersonalAcc() + "\nNumber of ISA Accounts: " +
-                ba1.getNoOfISAAcc() + "\nNumber of Business Accounts: " + ba1.getNoOfBusinessAcc() + "\nAccount Number: " + ba1.getAccountNumber() + "\nSort Code: " + ba1.getSortCode() + "\nTax: " +
-                ba1.getAccountType() + "\nUser Funds: " + ba1.getUserFunds() + "\nDirect Debit: " + ba1.isDirectDebit() + "\nStanding Orders: " + ba1.isStandingOrder() + "\nTax: " +
-                ba1.isTax() + "\nBusinessProof" +getBusinessProof() +"\nBannedBuisness" + getBannedBuisness() + "\nBusinessSector" getBusinessSector()+ "\nChequebookReq"
-                getChequebookReq() +"\nOverdraft" getOverdraft() +"\nLoanValue" getLoanValue() + "\n\n\n");
+        //output the results of default business Account (test)
+//        System.out.println("ID:" + ba1.getId() + "\nPhoto ID: " + ba1.getPhotoId() + "\nAddress ID:" + ba1.getAddressId() + "\nNumber of Personal Accounts: " + ba1.getNoOfPersonalAcc() + "\nNumber of ISA Accounts: " +
+//                ba1.getNoOfISAAcc() + "\nNumber of Business Accounts: " + ba1.getNoOfBusinessAcc() + "\nAccount Number: " + ba1.getAccountNumber() + "\nSort Code: " + ba1.getSortCode() + "\nTax: " +
+//                ba1.getAccountType() + "\nUser Funds: " + ba1.getUserFunds() + "\nDirect Debit: " + ba1.isDirectDebit() + "\nStanding Orders: " + ba1.isStandingOrder() + "\nTax: " +
+//                ba1.isTax() + "\nBusinessProof" +getBusinessProof() +"\nBannedBuisness" + getBannedBuisness() + "\nBusinessSector" getBusinessSector()+ "\nChequebookReq"
+//                getChequebookReq() +"\nOverdraft" getOverdraft() +"\nLoanValue" getLoanValue() + "\n\n\n");
+        // BUSINESS ACCOUNT DEFAULT DATA SECTION END
 
+        // ISA ACCOUNT DEFAULT DATA SECTION START
+        List iaList = new ArrayList<User>();
 
-        // PERSONAL ACCOUNT DEFAULT DATA SECTION END
+        ISAAccount ia1 = new ISAAccount(1,"tempPhotoId", "tempAddressId", 1, 3, 0, 1, 1, 'd', 5, true, false, false, true, false, true, true, false, 15000, 22);
+        ISAAccount ia2 = new ISAAccount(2,"tempPhotoId", "tempAddressId", 1, 4, 0, 1, 1, 'd', 5, false, false, false, true, true, true, true, true, 7777, 40);
+        ISAAccount ia3 = new ISAAccount(3,"tempPhotoId", "tempAddressId", 1, 2, 2, 4, 1, 'd', 5, true, false, false, true, false, true, false, false, 2000, 69);
+
+        paList.add(iaList);
+        paList.add(iaList);
+        paList.add(iaList);
+        // ISA ACCOUNT DEFAULT DATA SECTION END#
+
+        // LOGIN SYSTEM
+        String userName = "admin";
+        String password = "admin";
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter username: ");
+        String enteredUsername = reader.nextLine().toLowerCase().trim();
+        System.out.print("Enter password: ");
+        String enteredPassword = reader.nextLine();
+
+        if (userName.equals(enteredUsername) && (password.equals(enteredPassword))){
+            System.out.println("User Authorised");
+
+            // Menu for choosing an account
+            System.out.println(" ## Main Menu ## ");
+            System.out.println("1. Search Personal Account");
+            System.out.println("2. Search ISA Account");
+            System.out.println("3. Search Business Account");
+            System.out.println("x. Exit Program");
+
+            System.out.println("Make your entry (1, 2, 3, 4)");
+            int userEntry = reader.nextInt();
+            if (userEntry == 1) {
+                System.out.println("Enter ID: ");
+                int idEntry = reader.nextInt();
+                if (idEntry < paList.size()) {
+                    System.out.println(paList.get(userEntry - 1));
+                }
+            }
+            if (userEntry == 2) {
+                System.out.println("Enter ID: ");
+                int idEntry = reader.nextInt();
+                if (idEntry < iaList.size()) {
+                    System.out.println(iaList.get(userEntry - 1));
+                }
+            }
+            if (userEntry == 3) {
+                System.out.println("Enter ID: ");
+                int idEntry = reader.nextInt();
+                if (idEntry < baList.size()) {
+                    System.out.println(baList.get(userEntry - 1));
+                }
+            }
+            if (userEntry == 4) {
+                System.out.println("Exiting Program");
+                System.exit(0);
+            }
+        }
+        else {
+            System.out.println("Invalid username/password");
+        }
     }
-
 }
